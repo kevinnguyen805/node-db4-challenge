1ba1b765c99eae16876eeefb7d79a6968aa2ede1
admin <admin@admins-MacBook-Pro.local>
2019-11-07 12:06:08 -0800

    db config and knex migrations completed

--- data/migrations/20191107111238_recipe_book.js
+++ data/migrations/20191107111238_recipe_book.js
@@ -1,8 +1,36 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+     .createTable('recipes', tbl => {
+          tbl.increments()
+          tbl.string('name', 255).notNullable()
+     })
+     .createTable('ingredients', tbl => {
+          tbl.increments()
+          tbl.string('name', 255).notNullable()
+     })
+     .createTable('recipes_ingredients', tbl => {
+          tbl.increments()
+          tbl.integer('recipe_id')
+               .unsigned()
+               .notNullable()
+               .references('id')
+               .inTable('recipes')
+               .onDelete('RESTRICT')
+               .onUpdate('CASCADE')
+          tbl.integer('ingredient_id')
+               .unsigned()
+               .notNullable()
+               .references('id')
+               .inTable('ingredients')
+               .onDelete('RESTRICT')
+               .onUpdate('CASCADE')
+     })
 };
 
 exports.down = function(knex) {
-  
+     return knex.schema
+          .dropTableIfExists('recipes_ingredients')
+          .dropTableIfExists('ingredients')
+          .dropTableIfExists('recipes');
 };
